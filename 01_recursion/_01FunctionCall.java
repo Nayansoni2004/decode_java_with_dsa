3cd4417b7d6c084362529d1ce72398f0dafafdc7
NAYAN_SONI <nayansoni142004@gmail.com>
2025-03-14 23:30:57 +0530

    first commit

---
+++ 01_recursion/_01FunctionCall.java
@@ -0,0 +1,16 @@
+public class _01FunctionCall {
+    public static void apple() {
+        System.out.println("Hi, i am in apple");
+    }
+    public static void main(String[] args) {
+        System.out.println("Hi, i am in main method");
+        apple();
+    }
+}
+
+/*
+D:\decode_java_with_dsa\06_recursion> javac _01FunctionCall.java
+D:\decode_java_with_dsa\06_recursion> java _01FunctionCall
+Hi, i am in main method
+Hi, i am in apple
+ */
