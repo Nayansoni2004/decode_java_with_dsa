7304fd97090b4feab6b5ad3c5463354b2882556f
Nayansoni2004 <nayansoni142004@gmail.com>
2025-08-24 20:17:55 +0530

    build a func which calculates the factorial of n using recursion

---
+++ src/_01_recursion/_04Factorial.java
@@ -0,0 +1,30 @@
+//make a function which calculates the factorial of n using recursion?
+package  _01_recursion;
+
+import java.util.Scanner;
+
+public class _04Factorial {
+    public static int fact(int n) {
+        if(n == 1) 
+            return 1; //if this line runs then rest of lines will not run.
+        int ans = n * fact(n-1); //recursive call.
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+        System.out.println(fact(n));
+
+        sc.close();
+    }
+}
+/*
+D:\decode_java_with_dsa\06_recursion>java _04Factorial
+Enter n: 2
+2
+
+D:\decode_java_with_dsa\06_recursion>java _04Factorial
+Enter n: 5
+120
+ */
