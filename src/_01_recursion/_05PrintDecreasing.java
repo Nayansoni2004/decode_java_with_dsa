5ff7c530a4500cc4ca920e54b9fc39c22d6d8830
Nayansoni2004 <nayansoni142004@gmail.com>
2025-08-24 20:18:30 +0530

    print n to 1 using recursion

---
+++ src/_01_recursion/_05PrintDecreasing.java
@@ -0,0 +1,29 @@
+// print n to 1.
+package _01_recursion;
+
+import java.util.Scanner;
+
+public class _05PrintDecreasing {
+    public static void print(int n) {
+        if(n == 0) 
+            return;
+        System.out.println(n);
+        print(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+        print(n);
+        sc.close();
+    }
+}
+/*
+D:\github_repos\decode_java_with_dsa\01_recursion>java _05PrintDecreasing
+Enter n: 5
+5
+4
+3
+2
+1
+ */
