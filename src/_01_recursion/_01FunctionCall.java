072beff57e3879777f97f5a7df7e56e74d64b4b4
Nayansoni2004 <nayansoni142004@gmail.com>
2025-08-24 20:14:33 +0530

    execution starts from java main method

---
+++ src/_01_recursion/_01FunctionCall.java
@@ -0,0 +1,18 @@
+package _01_recursion;
+
+public class _01FunctionCall {
+    public static void apple() {
+        System.out.println("Hii, i am in apple");
+    }
+    public static void main(String[] args) {
+        System.out.println("Hii, i am in main method");
+        apple();
+    }
+}
+
+/*
+D:\decode_java_with_dsa\06_recursion> javac _01FunctionCall.java
+D:\decode_java_with_dsa\06_recursion> java _01FunctionCall
+Hi, i am in main method
+Hi, i am in apple
+ */
